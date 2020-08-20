d08fc5b9301fa87cee4bdd37066cb41f33ceb7de
Abhishek Vijayakumar <abhishev@andrew.cmu.edu>
2020-08-19 22:09:02 -0400

    Initial novel class

---
+++ novel.py
@@ -0,0 +1,40 @@
+from typing import Dict, List, Optional, Tuple
+
+
+# Typedefs
+# TODO: document each typedef
+Link = str
+Path = str
+Chapter = Dict[str, Tuple[Link, Optional[List[str]]]]
+
+
+class Novel:
+    """
+    Represents a novel.
+    """
+
+    def __init__(self, link: Link) -> None:
+        """
+        Initializes a new Novel.
+        :param link: Link to the novelupdates source page for the Novel.
+        :return None.
+        """
+        # TODO: put metadata here
+        self.chapters: List[Chapter] = []
+        raise NotImplementedError
+
+    def __getitem__(self, key: int) -> Chapter:
+        """
+        Returns the chapter specified by the index.
+        :param key: The index of the chapter to return.
+        :return: The indicated chapter.
+        """
+        return self.chapters[key]
+
+    def _build_novel(self, path: Path) -> None:
+        """
+        Builds an ebook for the given novel and returns a path to the novel.
+        :param path: The directory in which to create the ebook.
+        :return: A path to the built ebook.
+        """
+        raise NotImplementedError
