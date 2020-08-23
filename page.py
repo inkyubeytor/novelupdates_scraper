dbc1cc2c71f7416e1199a6ee9eefa3a85b9126fe
Abhishek Vijayakumar <abhishev@andrew.cmu.edu>
2020-08-23 00:08:51 -0400

    updated page methods

--- page.py
+++ page.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class Page:
     """
     Represents a single translation of a chapter.
@@ -8,8 +11,20 @@ class Page:
         :param link: A link to the page.
         """
         self.link = link
+        self.contents = None
 
-    def scrape(self) -> None:
+    def get(self, no_cache: bool) -> str:
+        """
+        Returns page data, scraping it if it is not available.
+        :param no_cache: Whether to force rescraping of cached chapters.
+        :return: If the page is already scraped, returns the scraped data, else
+        scrapes the data and returns it.
+        """
+        if self.contents is None or no_cache:
+            self._scrape()
+        return self.contents
+
+    def _scrape(self) -> None:
         """
         Downloads the page data and scrapes text.
         :return: None.
